534fc3e3827af081be09b06813d772939cd12e6b
BenseddikM <benseddikmohammed@gmail.com>
2017-09-14 14:48:35 +0200

    Settings modified

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r^ue0!5za$!(vintplp$@%zjmo1@$9itdryo9*_$3e)*0jye-2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mbenseddik.pythonanywhere.com"]
 
 
 # Application definition
